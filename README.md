f6ccf64eafe8a4923ad9e7f921a479512e9d4c6c
Javi Cuenca <jcuencagento@gmail.com>
2023-11-30 01:05:46 +0100

    Fourth week...

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
    <img align="center" src="./public/codember.png" />
 </p>
 
- <h2> - Codember challenges from https://codember.dev/ </h2>
+ <h2> - Codember challenges from https://codember.dev/ made by https://github.com/midudev !</h2>
 
 
 [🚀 Challenge 01](https://github.com/jcuencagento/codemberJCG/blob/master/challenge_01/challenge_01.md)
@@ -12,3 +12,5 @@
 [🖥️ Challenge 02](https://github.com/jcuencagento/codemberJCG/blob/master/challenge_02/challenge_02.md)
 
 [🔐 Challenge 03](https://github.com/jcuencagento/codemberJCG/blob/master/challenge_03/challenge_03.md)
+
+[📋 Challenge 04](https://github.com/jcuencagento/codemberJCG/blob/master/challenge_04/challenge_04.md)
--- challenge_04/challenge_04.js
+++ challenge_04/challenge_04.js
@@ -1,1 +1,48 @@
-/* Wating... */
+const fs = require('fs');
+
+function checkChecksum (file) {
+    const [ filename, checksum ] = file.split('-');
+    const array_filename = filename.trim().split('');
+    const set_filename = [...new Set(array_filename)];
+    let repeated = [];
+    set_filename.forEach(char => {
+        if (array_filename.filter(c => c === char).length > 1) {
+            repeated.push(char);
+        }
+    });
+
+    const real_checksum = set_filename.filter(c => !repeated.includes(c)).join('');
+    return { valid: checksum === real_checksum, checksum: checksum };
+}
+
+async function checkFiles (file, cb) {
+    let valid_files = [];
+    let not_valid_files = [];
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            return cb(err);
+        }
+    
+        data.toString().split(/\r?\n/).forEach(line =>  {
+            const result = checkChecksum(line);
+            if (result.valid) {
+                valid_files.push(result.checksum);
+            } else {
+                not_valid_files.push(result.checksum);
+            }
+        });
+        
+        return cb(null, { valid_files, not_valid_files });
+    });
+}
+
+checkFiles('./challenge_04/files_quarantine.txt', (err, result_challenge_04) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    
+    console.log('Challenge 04:');
+    console.log(result_challenge_04.valid_files[32]);
+    return;
+});
